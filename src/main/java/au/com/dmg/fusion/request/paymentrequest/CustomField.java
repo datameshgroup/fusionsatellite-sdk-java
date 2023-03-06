e24ae98443be5918d8fcb7f015dbcc0e8f90fdd4
Michael Johnston <michael@datameshgroup.com>
2023-03-06 16:53:21 +1100

    added field type testing

--- src/main/java/au/com/dmg/fusion/request/paymentrequest/CustomField.java
+++ src/main/java/au/com/dmg/fusion/request/paymentrequest/CustomField.java
@@ -3,7 +3,8 @@ package au.com.dmg.fusion.request.paymentrequest;
 import au.com.dmg.fusion.data.CustomFieldType;
 import com.squareup.moshi.Json;
 import org.jetbrains.annotations.NotNull;
-import java.lang.reflect.Array;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class CustomField {
@@ -52,34 +53,33 @@ public class CustomField {
             this.type = type;
             return Builder.this;
         }
+
+        /**
+         * @param value the value should be valid JSON, serialized as a String.
+         *              e.g [1,2,3,4] should be "[1,2,3,4]", with a type of Array
+         *              true should be "true" with a type of Boolean
+         *              321.5 should be "321.5" with a type of Number
+         *              {a:"hello world"} should be "{a:\"hello world\"}"
+         * @return
+         */
         public Builder value(String value) {
             this.value = value;
             return Builder.this;
         }
 
-        public Builder value(Integer value) {
-            this.value = value.toString();
+        public Builder value(int value) {
+            this.value = Integer.toString(value);
             this.type = CustomFieldType.Integer;
             return Builder.this;
         }
-        public Builder value(Array value) {
-            this.value = value.toString();
-            this.type = CustomFieldType.Array;
-            return Builder.this;
-        }
-        public Builder value(Number value) {
-            this.value = value.toString();
+
+        public Builder value(double value) {
+            this.value = Double.toString(value);
             this.type = CustomFieldType.Number;
             return Builder.this;
         }
-        public Builder value(Object value) {
-            this.value = value.toString();
-            this.type = CustomFieldType.Object;
-            return Builder.this;
-        }
-
-        public Builder value(Boolean value) {
-            this.value = value.toString();
+        public Builder value(boolean value){
+            this.value = Boolean.toString(value);
             this.type = CustomFieldType.Boolean;
             return Builder.this;
         }
@@ -100,7 +100,40 @@ public class CustomField {
                         + "Please set the Value by \"Value()\". "
                         + "the properties \"Key\", \"Type\", and \"Value\" are required.");
             }
-            
+
+            if (this.type == CustomFieldType.Array){
+                try{
+                    JSONArray jsonArray = new JSONArray(this.value);
+                } catch (Exception e){
+                    throw new IllegalArgumentException("Error " + this.value + " is not a valid array message: " + e.getMessage());
+                }
+
+            } else if (this.type == CustomFieldType.Boolean){
+                if(!this.value.equals("true") && !this.value.equals("false")){
+                    throw new IllegalArgumentException("Error " + this.value + " must either be 'true' or 'false'");
+                }
+            } else if (this.type == CustomFieldType.Integer){
+                try{
+                    Integer i = Integer.valueOf(this.value);
+                } catch (NumberFormatException err){
+                    throw new IllegalArgumentException("Error " + this.value + " is not a valid integer.");
+                }
+            } else if (this.type == CustomFieldType.Number){
+                try {
+                    Double d = Double.valueOf(this.value);
+                } catch (NumberFormatException err){
+                    throw new IllegalArgumentException("Error " + this.value + " is not a valid decimal point number");
+                }
+            } else if (this.type == CustomFieldType.Object){
+                try{
+                    JSONObject obj = new JSONObject(this.value);
+                } catch (Exception e){
+                    throw new IllegalArgumentException("Error " + this.value + " is not a valid JSON Object: " + e.getMessage());
+                }
+            } else if (this.type == CustomFieldType.Unknown){
+                throw new IllegalArgumentException("Error unknown type used for data. do not used this.");
+            }
+
             return new CustomField(this);
         }
     }
