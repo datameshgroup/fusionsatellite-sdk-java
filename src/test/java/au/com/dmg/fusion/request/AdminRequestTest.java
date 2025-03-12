73dc9ca0fc542df517cdfb028f95c0f6bf7d9641
Vanessa Agustin <vanessa@datameshgroup.com>
2025-03-12 15:06:13 +1100

    Version 2.0.15 - added Verify to StoredValueTransactionType

--- src/test/java/au/com/dmg/fusion/request/AdminRequestTest.java
+++ src/test/java/au/com/dmg/fusion/request/AdminRequestTest.java
@@ -69,31 +69,6 @@ public class AdminRequestTest {
     }
 
     @Test
-    public void testValidWithMessageReference(){
-        AdminRequest adminRequest = new AdminRequest.Builder()
-                .serviceIdentification(ServiceIdentification.MessageACK)
-                .messageReference(new MessageReference.Builder()
-                        .messageCategory(MessageCategory.Payment)
-                        .serviceID("OriginalServiceId123")
-                        .build())
-                .build();
-        Moshi moshi = new Moshi.Builder()
-                .add(new InstantAdapter())
-                .build();
-        JsonAdapter<AdminRequest> jsonAdapter = moshi.adapter(AdminRequest.class);
-        String json = jsonAdapter.toJson(adminRequest);
-        System.out.println(json);
-
-        AdminRequest serializedRequest = null;
-        try {
-            serializedRequest = jsonAdapter.fromJson(json);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assert (serializedRequest.getServiceIdentification().toString().equals("MessageACK"));
-    }
-
-    @Test
     public void testNullServiceIdentification(){
         NullPointerException exception =
                 assertThrows(NullPointerException.class,
