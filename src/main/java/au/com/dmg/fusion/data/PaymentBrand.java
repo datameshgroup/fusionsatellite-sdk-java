b38f66b2130469586908d22f5cbcc7a5a193a0bd
Grace <grace@datameshgroup.com>
2026-09-08 11:34:14 +1000

    TTF-126 Add Mobil Card Support.

--- src/main/java/au/com/dmg/fusion/data/PaymentBrand.java
+++ src/main/java/au/com/dmg/fusion/data/PaymentBrand.java
@@ -68,6 +68,7 @@ public enum PaymentBrand {
     @Json(name = "Caltex StarCard") CaltexStarCard,
     @Json(name = "United Fuel Card") UnitedFuelCard,
     @Json(name = "OTR Card") OTRCard,
+    @Json(name = "Mobil Fuel Card") MobilFuelCard,            
     @Json(name = "CPVV MPTP") CPVVMPTP,
     @Json(name = "Qantas") Qantas,
     @Json(name = "DRC") DRC,
